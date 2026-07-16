63a17442cee783bf26a8038796ebaaad09950647
satella <forgottensatella@gmail.com>
2026-07-16 14:40:17 +0300

    Random Commit

--- tree-installer-system/grub-theme/run.sh
+++ tree-installer-system/grub-theme/run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-ROOT_UID=0
-HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 THEME_DIR="/boot/grub/themes"
 THEME_NAME="satellaos-grub-theme-polaris"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ "$UID" -eq "$ROOT_UID" ]; then
+if [ "$UID" -eq 0 ]; then
   [[ -d "${THEME_DIR}/${THEME_NAME}" ]] && rm -rf "${THEME_DIR}/${THEME_NAME}"
   mkdir -p "${THEME_DIR}/${THEME_NAME}"
-  rsync -a --delete "${SCRIPT_DIR}/${THEME_NAME}/" "${THEME_DIR}/${THEME_NAME}/"
+  cp -a "${SCRIPT_DIR}/${THEME_NAME}/." "${THEME_DIR}/${THEME_NAME}/"
   cp -an /etc/default/grub /etc/default/grub.bak
   grep -q "GRUB_THEME=" /etc/default/grub && sed -i '/GRUB_THEME=/d' /etc/default/grub
   echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
