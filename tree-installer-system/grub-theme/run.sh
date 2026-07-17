ef935c0d107199ef89abef973d034606968eaa39
satella <forgottensatella@gmail.com>
2026-07-17 12:17:54 +0300

    Update GRUB Theme

--- tree-installer-system/grub-theme/run.sh
+++ tree-installer-system/grub-theme/run.sh
@@ -8,12 +8,12 @@ if [ "$UID" -eq 0 ]; then
   [[ -f /boot/grub/splash.png ]] && sudo mv /boot/grub/splash.png /boot/grub/splash-backup.png
 
   [[ -d "${THEME_DIR}/${THEME_NAME}" ]] && rm -rf "${THEME_DIR}/${THEME_NAME}"
-  mkdir -p "${THEME_DIR}/${THEME_NAME}"
-  cp -a "${SCRIPT_DIR}/${THEME_NAME}/." "${THEME_DIR}/${THEME_NAME}/"
-  cp -an /etc/default/grub /etc/default/grub.bak
-  grep -q "GRUB_THEME=" /etc/default/grub && sed -i '/GRUB_THEME=/d' /etc/default/grub
-  echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
-  update-grub
+  sudo mkdir -p "${THEME_DIR}/${THEME_NAME}"
+  sudo cp -a "${SCRIPT_DIR}/${THEME_NAME}/." "${THEME_DIR}/${THEME_NAME}/"
+  sudo cp -an /etc/default/grub /etc/default/grub.bak
+  sudo grep -q "GRUB_THEME=" /etc/default/grub && sed -i '/GRUB_THEME=/d' /etc/default/grub
+  sudo echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
+  sudo update-grub
 else
   sudo "$0"
 fi
