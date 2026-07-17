4cb8e191174ccdaa42ee6bd5103466a7a48d77aa
satella <forgottensatella@gmail.com>
2026-07-17 19:11:15 +0300

    Fixing Configuration Script

--- tree-installer-system/update-configuration/run.sh
+++ tree-installer-system/update-configuration/run.sh
@@ -2,10 +2,10 @@
 
 source "$HOME/.satellaos-source/installer"
 
-sudo cp -r $script_source/update-etc-skel-configuration/backup/.config/ /etc/skel/
+sudo cp -r $script_source/update-configuration/backup/.config/ /etc/skel/
 
-sudo cp $script_source/update-etc-skel-configuration/backup/.bashrc /etc/skel/.bashrc
+sudo cp $script_source/update-configuration/backup/.bashrc /etc/skel/.bashrc
 
-cp -r $script_source/update-etc-skel-configuration/backup/.config/ $HOME/
+cp -r $script_source/update-configuration/backup/.config/ $HOME/
 
-cp $script_source/update-etc-skel-configuration/backup/.bashrc $HOME.bashrc
+cp $script_source/update-configuration/backup/.bashrc $HOME.bashrc
