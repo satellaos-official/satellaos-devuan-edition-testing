a4ed4e5ae56c3ac575c4525f1cc739ac7caf5f84
satella <forgottensatella@gmail.com>
2026-07-17 12:24:03 +0300

    Update GRUB Theme

--- tree-installer-system/grub-theme/run.sh
+++ tree-installer-system/grub-theme/run.sh
@@ -4,16 +4,12 @@ THEME_DIR="/boot/grub/themes"
 THEME_NAME="satellaos-grub-theme-polaris"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ "$UID" -eq 0 ]; then
-  [[ -f /boot/grub/splash.png ]] && sudo mv /boot/grub/splash.png /boot/grub/splash-backup.png
+[[ -f /boot/grub/splash.png ]] && sudo mv /boot/grub/splash.png /boot/grub/splash-backup.png
 
-  [[ -d "${THEME_DIR}/${THEME_NAME}" ]] && rm -rf "${THEME_DIR}/${THEME_NAME}"
-  sudo mkdir -p "${THEME_DIR}/${THEME_NAME}"
-  sudo cp -a "${SCRIPT_DIR}/${THEME_NAME}/." "${THEME_DIR}/${THEME_NAME}/"
-  sudo cp -an /etc/default/grub /etc/default/grub.bak
-  sudo grep -q "GRUB_THEME=" /etc/default/grub && sed -i '/GRUB_THEME=/d' /etc/default/grub
-  sudo echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
-  sudo update-grub
-else
-  sudo "$0"
-fi
+[[ -d "${THEME_DIR}/${THEME_NAME}" ]] && sudo rm -rf "${THEME_DIR}/${THEME_NAME}"
+sudo mkdir -p "${THEME_DIR}/${THEME_NAME}"
+sudo cp -a "${SCRIPT_DIR}/${THEME_NAME}/." "${THEME_DIR}/${THEME_NAME}/"
+sudo cp -an /etc/default/grub /etc/default/grub.bak
+grep -q "GRUB_THEME=" /etc/default/grub && sudo sed -i '/GRUB_THEME=/d' /etc/default/grub
+echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" | sudo tee -a /etc/default/grub > /dev/null
+sudo update-grub
